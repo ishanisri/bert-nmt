8f03953c60fc5633a87363ca741bb9d9f856f890
Ishani Srivastava <ishasri.1212@gmail.com>
2021-03-07 09:50:00 +0530

    Update path of test, valid and train

--- examples/translation/makedataforbert.sh
+++ examples/translation/makedataforbert.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 lng=$1
+path=$2
 echo "src lng $lng"
-for sub  in train valid test
+for sub  in $2/train $2/valid $2/test
 do
     sed -r 's/(@@ )|(@@ ?$)//g' ${sub}.${lng} > ${sub}.bert.${lng}.tok
     ../mosesdecoder/scripts/tokenizer/detokenizer.perl -l $lng < ${sub}.bert.${lng}.tok > ${sub}.bert.${lng}

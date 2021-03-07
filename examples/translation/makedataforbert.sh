9465cb0d5154bf79a76cebc2734acd822699a6bd
Ishani Srivastava <ishasri.1212@gmail.com>
2021-03-07 09:55:25 +0530

    Remove changes after copying files

--- examples/translation/makedataforbert.sh
+++ examples/translation/makedataforbert.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 lng=$1
-path=$2
 echo "src lng $lng"
-for sub  in $2/train $2/valid $2/test
+for sub  in train valid test
 do
     sed -r 's/(@@ )|(@@ ?$)//g' ${sub}.${lng} > ${sub}.bert.${lng}.tok
-    mosesdecoder/scripts/tokenizer/detokenizer.perl -l $lng < ${sub}.bert.${lng}.tok > ${sub}.bert.${lng}
+    ../mosesdecoder/scripts/tokenizer/detokenizer.perl -l $lng < ${sub}.bert.${lng}.tok > ${sub}.bert.${lng}
     rm ${sub}.bert.${lng}.tok
 done

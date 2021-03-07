7ed127e8ed02e06e6f20b5da4e0ce27ceae66337
Ishani Srivastava <ishasri.1212@gmail.com>
2021-03-07 09:52:25 +0530

    Update path of mosesnecoder

--- examples/translation/makedataforbert.sh
+++ examples/translation/makedataforbert.sh
@@ -5,6 +5,6 @@ echo "src lng $lng"
 for sub  in $2/train $2/valid $2/test
 do
     sed -r 's/(@@ )|(@@ ?$)//g' ${sub}.${lng} > ${sub}.bert.${lng}.tok
-    ../mosesdecoder/scripts/tokenizer/detokenizer.perl -l $lng < ${sub}.bert.${lng}.tok > ${sub}.bert.${lng}
+    mosesdecoder/scripts/tokenizer/detokenizer.perl -l $lng < ${sub}.bert.${lng}.tok > ${sub}.bert.${lng}
     rm ${sub}.bert.${lng}.tok
 done
